c08e9d2678964e31ecb06d0083420128931ae679
TOOFACK <paulrussianvodka@gmail.com>
2020-04-28 19:08:59 +0500

    Initial commit

---
+++ src/main/java/com/coursework/CourseWork/repo/PostRepository.java
@@ -0,0 +1,9 @@
+package com.coursework.CourseWork.repo;
+
+import com.coursework.CourseWork.Models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+
+}
